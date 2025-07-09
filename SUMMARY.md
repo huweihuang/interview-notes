b089a47461d8804dffc4ce41122712f58b9dc1d0
huweihuang <huweihuang@foxmail.com>
2025-07-09 13:46:40 +0800

    init gitbook

---
+++ SUMMARY.md
@@ -0,0 +1,31 @@
+# Summary
+
+## 目录 <a id="introduction"></a>
+
+- [序言](README.md)
+
+## Linux基础 <a id="linux"></a>
+
+- [Linux基础](linux/linux.md)
+
+## 数据结构 <a id="data-structure"></a>
+
+- [数据结构基础](data-structure/data-structure.md)
+
+## TCP/IP <a id="tcpip"></a>
+
+- [TCP/IP基础](tcpip/tcpip.md)
+
+## Golang <a id="golang"></a>
+
+- [Golang基础](golang/golang-basic.md)
+- [Golang进阶](golang/golang-advanced.md)
+
+## Kubernetes <a id="k8s"></a>
+
+- [k8s基础](k8s/k8s-basic.md)
+- [k8s进阶](k8s/k8s-advanced.md)
+
+## 算法题 <a id="leetcode"></a>
+
+- [LeetCode](leetcode/leetcode.md)
---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -x
+set -e
+
+MESSAGE=$1
+DEPLOY_MODE=$2
+
+# update blog in blog-source repo
+NOTE_NAME="interview-notes"
+BLOG_SOURCE_DIR="../blog-source/${NOTE_NAME}"
+MASTER_DIR="$(pwd)"
+GHPAGE_DIR="${MASTER_DIR}/gh-pages"
+
+# clean GHPAGE_DIR
+if [ ! -d $GHPAGE_DIR  ];then
+    git clone -b gh-pages https://github.com/huweihuang/${NOTE_NAME}.git gh-pages
+fi
+rm -fr ${GHPAGE_DIR}/*
+
+# build
+gitbook build
+
+# copy _book to GHPAGE_DIR
+cp -fr ${MASTER_DIR}/_book/* ${GHPAGE_DIR}
+cp -fr ${MASTER_DIR}/README.md ${GHPAGE_DIR}
+cp -fr ${MASTER_DIR}/SUMMARY.md ${GHPAGE_DIR}
+
+if [ $2 = "dry-run" ];then
+    echo "gitbook server dry run"
+    gitbook serve
+else
+    # git commit
+    cd ${GHPAGE_DIR}
+    git add --all
+    git config user.name huweihuang
+    git config user.email huweihuang@foxmail.com
+    git commit -m "${MESSAGE}"
+    git push origin gh-pages
+fi
